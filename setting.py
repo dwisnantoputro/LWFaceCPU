d29c951fb74f9631df7ba78b3a56f59dcc2e23d4
dwisnantoputro <dwisnantoputro@gmail.com>
2021-04-07 10:54:40 +0900

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,13 @@
+# setting.py
+
+stg = {
+    'webcam': True,
+    'size_w' : 640,
+    'size_h' : 480,
+    'record': True,
+    'frames' : 500,
+    'video': False,
+    'video_file': 'Video/vid3.mp4',
+    'image' : False,
+    'image_file': 'image/1.jpg',
+}
